5c26314aef6092ae15bb5d3ae5c64db30922edf5
manuelsanchez2 <manusansan22@gmail.com>
2020-08-14 12:01:07 +0200

    Add Search input and main bg colour

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./App.css";
+import logoSrc from "./assets/logo.png";
 
 function App() {
-  return <h2>Hola</h2>;
+  return (
+    <div>
+      <header className="header">
+        <h1>WHISKEYMANIACS</h1>
+        <img className="app__logo" src={logoSrc} alt="whiskeymaniacs" />
+        <input className="input" placeholder="Get your whisky..." />
+      </header>
+      <main className="main">HOLA</main>
+      <footer></footer>
+    </div>
+  );
 }
 
 export default App;
