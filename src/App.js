ff32596a59b6325a9a852fafdefb2ded9ada71c0
manuelsanchez2 <manusansan22@gmail.com>
2020-08-14 13:42:05 +0200

    Add list components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from "react";
 import "./App.css";
 import logoSrc from "./assets/logo.png";
+import List from "./components/List";
+import ListItems from "./components/ListItems";
+import ListItemImage from "./components/ListItemImage";
+import ListItemText from "./components/ListItemText";
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <header className="header">
         <h1>WHISKEYMANIACS</h1>
         <img className="app__logo" src={logoSrc} alt="whiskeymaniacs" />
         <input className="input" placeholder="Get your whisky..." />
       </header>
-      <main className="main">HOLA</main>
+      <main className="main">
+        <List>
+          <ListItems>
+            <ListItemImage src={"logoSrc"} alt="Whiskeymaniacs logo" />
+            <ListItemText title={""} description={""}></ListItemText>
+          </ListItems>
+        </List>
+      </main>
       <footer></footer>
     </div>
   );
---
+++ src/components/ListItemImage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ListItemImage({ src, alt }) {
+  return <img src={src} alt={alt} />;
+}
+
+export default ListItemImage;
