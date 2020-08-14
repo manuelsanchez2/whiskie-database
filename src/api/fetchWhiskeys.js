fd9d944600f6bf5602035e3406dc34e4c8e40d83
manuelsanchez2 <manusansan22@gmail.com>
2020-08-14 17:03:27 +0200

    Load API with the page

--- src/api/fetchWhiskeys.js
+++ src/api/fetchWhiskeys.js
@@ -1,11 +1,12 @@
-export async function fetchWhiskeys() {
+export default async function fetchWhiskeys() {
   const response = await fetch(
-    "https://evening-citadel-85778.herokuapp.com:443/whiskey/27"
+    "https://evening-citadel-85778.herokuapp.com:443/whiskey/"
   );
   if (!response.ok) {
     throw new Error(response);
   }
   const data = await response.json();
+  console.log(data);
   const whiskeys = data.results.map((whiskey) => ({
     title: whiskey.title,
     description: whiskey.description,
