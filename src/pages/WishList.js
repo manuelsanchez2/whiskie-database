a7b273136682827e56fa363c92e1f812b51ae530
manuelsanchez2 <manusansan22@gmail.com>
2020-08-21 16:57:26 +0200

    Implement useQuery with own db.json

--- src/pages/WishList.js
+++ src/pages/WishList.js
@@ -4,14 +4,19 @@ import StyledFooter from "../components/StyledFooter";
 import HeaderTitle from "../components/header/HeaderTitle";
 import HeaderImage from "../components/header/HeaderImage";
 import { useQuery } from "react-query";
+import WhiskeyListItemInfoButton from "../components/list/WhiskeyListItemInfoButton";
 
-const fetchMyWhiskey = async () => {
-  const response = await fetch("http://localhost:3333/favourites");
+const fetchMyWhiskie = async () => {
+  const response = await fetch("http://localhost:3333/whiskie");
   return response.json();
 };
 
 function WishList(props) {
-  const {} = useQuery("whiskies", fetchMyWhiskey);
+  const { data, status } = useQuery("whiskie", fetchMyWhiskie);
+  console.log(data);
+
+  // MY WHISKIES
+
   return (
     <>
       <StyledHeader>
@@ -19,7 +24,16 @@ function WishList(props) {
         <HeaderImage />
       </StyledHeader>
       <main>
-        <p>Here you have your whiskies</p>
+        {/* <p>{status}</p> */}
+        {status === "loading" && <div>Your whiskies are being loaded...</div>}
+        {status === "error" && <div>Error fetching data</div>}
+        {status === "success" && (
+          <div>
+            {data.map((whiskie) => (
+              <div>{whiskie.title}</div>
+            ))}
+          </div>
+        )}
       </main>
       <StyledFooter />
     </>
