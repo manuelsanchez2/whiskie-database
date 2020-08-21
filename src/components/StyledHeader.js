71f4eab03f464fee42bd6f3e4ce1aad79d2b603d
manuelsanchez2 <manusansan22@gmail.com>
2020-08-21 13:32:06 +0200

    End the StyledHeader component with JSS

--- src/components/StyledHeader.js
+++ src/components/StyledHeader.js
@@ -2,10 +2,13 @@ import React from "react";
 import styled from "@emotion/styled";
 
 const Header = styled.header`
-  display: flex;
-  align-content: center;
-  justify-content: center;
+  display: grid;
+  place-items: center;
+  font-size: 0.7em;
   background: var(--bg-gradient);
+  border-bottom-left-radius: 3px;
+  border-bottom-right-radius: 3px;
+  padding-bottom: 5px;
 `;
 function StyledHeader({ children }) {
   return <Header>{children}</Header>;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
 import React from "react";
 import WhiskeyList from "../components/WhiskeyList";
-import HeaderList from "../components/header/HeaderList";
 import StyledHeader from "../components/StyledHeader";
 import StyledFooter from "../components/StyledFooter";
+import HeaderInput from "../components/header/HeaderInput";
+import HeaderTitle from "../components/header/HeaderTitle";
+import HeaderImage from "../components/header/HeaderImage";
 
 function Home(props) {
   const [query, setQuery] = React.useState("");
@@ -10,7 +12,9 @@ function Home(props) {
   return (
     <>
       <StyledHeader>
-        <HeaderList
+        <HeaderTitle>WhiskeyManiacs</HeaderTitle>
+        <HeaderImage />
+        <HeaderInput
           whiskyQuery={query}
           onWhiskyQueryChange={(whiskyQuery) => setQuery(whiskyQuery)}
         />
