6a5936d6d4cc9ad887df48555facb7b071450964
manuelsanchez2 <manusansan22@gmail.com>
2020-08-21 16:21:33 +0200

    Add useQuery dependency

--- src/pages/WishList.js
+++ src/pages/WishList.js
@@ -3,8 +3,15 @@ import StyledHeader from "../components/StyledHeader";
 import StyledFooter from "../components/StyledFooter";
 import HeaderTitle from "../components/header/HeaderTitle";
 import HeaderImage from "../components/header/HeaderImage";
+import { useQuery } from "react-query";
+
+const fetchMyWhiskey = async () => {
+  const response = await fetch("http://localhost:3333/favourites");
+  return response.json();
+};
 
 function WishList(props) {
+  const {} = useQuery("whiskies", fetchMyWhiskey);
   return (
     <>
       <StyledHeader>
