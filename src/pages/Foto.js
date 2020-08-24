5e2036020d2119038a7646a42b757ef2eee39d78
manuelsanchez2 <manusansan22@gmail.com>
2020-08-24 08:43:21 +0200

    Improve camara functionalities with image preview

--- src/pages/Foto.js
+++ src/pages/Foto.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import StyledHeader from "../components/StyledHeader";
 import StyledFooter from "../components/StyledFooter";
 import HeaderTitle from "../components/header/HeaderTitle";
@@ -7,10 +7,15 @@ import StyledMain from "../components/StyledMain";
 import Camera from "react-html5-camera-photo";
 import "react-html5-camera-photo/build/css/index.css";
 
+import ImagePreview from "./ImagePreview";
+
 function Foto(props) {
+  const [dataUri, setDataUri] = useState("");
   function handleTakePhoto(dataUri) {
     console.log("take picture");
+    setDataUri(dataUri);
   }
+  const isFullScreen = false;
   return (
     <>
       <StyledHeader>
@@ -19,11 +24,14 @@ function Foto(props) {
       </StyledHeader>
       <StyledMain>
         <p>Camera functions in progress...</p>
-        <Camera
-          onTakePhoto={(dataUri) => {
-            handleTakePhoto(dataUri);
-          }}
-        />
+        {dataUri ? (
+          <ImagePreview dataUri={dataUri} isFullScreen={isFullScreen} />
+        ) : (
+          <Camera
+            onTakePhotoAnimationDone={handleTakePhoto}
+            isFullScreen={isFullScreen}
+          />
+        )}
       </StyledMain>
       <StyledFooter />
     </>
